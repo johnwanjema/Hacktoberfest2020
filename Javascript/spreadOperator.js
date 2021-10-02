c11fd18d168a7edfe3bd502c45818f2dca30b559
John Wanjema <49660390+johnwanjema@users.noreply.github.com>
2021-10-02 11:23:50 +0300

    add spread operator code

---
+++ Javascript/spreadOperator.js
@@ -0,0 +1,11 @@
+Allows an iterable such as an array expression or string to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected, 
+or an object expression to be expanded in places where zero or more key-value pairs (for object literals) are expected.
+
+//sum the arguments of a function
+function sum(x, y, z) {
+  return x + y + z;
+}
+
+const numbers = [1, 2, 3];
+
+console.log(sum(...numbers));
